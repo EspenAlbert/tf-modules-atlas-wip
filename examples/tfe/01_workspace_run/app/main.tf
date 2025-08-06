c4bf3153a79707466bd76e23da8e9f98f2e9e448
Espen Albert <espen.albert@mongodb.com>
2025-08-06 09:59:12 +0100

    feat: Implement conditional TFE workspace runs for VPC endpoint creation and update local variables for improved logic

--- examples/tfe/01_workspace_run/app/main.tf
+++ examples/tfe/01_workspace_run/app/main.tf
@@ -12,12 +12,10 @@ resource "mongodbatlas_privatelink_endpoint" "mongodb_endpoint" { # Creation Ord
   region        = var.aws_region
 }
 
-output "atlas_endpoint_service_name" {
-  value = mongodbatlas_privatelink_endpoint.mongodb_endpoint.endpoint_service_name
-}
 
 locals {
-  vpc_endpoint_id = try(data.tfe_outputs.platform[0].values.aws_vpc_endpoint_id, "")
+  vpc_endpoint_id      = try(data.tfe_outputs.platform[0].values.aws_vpc_endpoint_id, "")
+  missing_vpc_endpoint = local.vpc_endpoint_id == ""
 }
 
 resource "mongodbatlas_privatelink_endpoint_service" "private_endpoint" { # Creation Order 3
@@ -27,4 +25,22 @@ resource "mongodbatlas_privatelink_endpoint_service" "private_endpoint" { # Crea
   private_link_id     = mongodbatlas_privatelink_endpoint.mongodb_endpoint.private_link_id
   endpoint_service_id = local.vpc_endpoint_id
   provider_name       = "AWS"
+}
+
+data "tfe_workspace" "platform" {
+  count = local.missing_vpc_endpoint ? 1 : 0
+
+  name         = var.platform_workspace
+  organization = var.tfe_org
+}
+
+resource "tfe_workspace_run" "platform_run" {
+  count = local.missing_vpc_endpoint ? 1 : 0
+
+  workspace_id = data.tfe_workspace.platform[0].id
+
+  apply {
+    manual_confirm = true
+    wait_for_run   = false
+  }
 }
--- examples/tfe/01_workspace_run/platform/main.tf
+++ examples/tfe/01_workspace_run/platform/main.tf
@@ -7,8 +7,20 @@ data "tfe_outputs" "app" {
   workspace    = var.app_workspace
 }
 
+data "aws_vpc_endpoint" "existing" {
+
+
+}
+data "tfe_outputs" "platform" {
+  workspace    = "vcs-platform"
+  organization = var.tfe_org
+}
+
 locals {
   atlas_endpoint_service_name = try(data.tfe_outputs.app[0].values.atlas_endpoint_service_name, "")
+  create_aws_vpc_endpoint     = local.atlas_endpoint_service_name != "" && var.app_workspace != ""
+  vpc_endpoint_is_new         = try(data.tfe_outputs.platform.values.aws_vpc_endpoint_id, "") == ""
+  trigger_app_run             = local.create_aws_vpc_endpoint && local.vpc_endpoint_is_new
 }
 
 
@@ -26,6 +38,20 @@ resource "aws_vpc_endpoint" "aws_endpoint" { # Creation Order 2
   service_name = local.atlas_endpoint_service_name
 }
 
-output "aws_vpc_endpoint_id" {
-  value = try(aws_vpc_endpoint.aws_endpoint[0].id, "")
+data "tfe_workspace" "app" {
+  count = local.trigger_app_run ? 1 : 0
+
+  name         = var.app_workspace
+  organization = var.tfe_org
+}
+
+resource "tfe_workspace_run" "app_run" {
+  count = local.trigger_app_run ? 1 : 0
+
+  workspace_id = data.tfe_workspace.app[0].id
+
+  apply {
+    manual_confirm = true
+    wait_for_run   = false
+  }
 }
