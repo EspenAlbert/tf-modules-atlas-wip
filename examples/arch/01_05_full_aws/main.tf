915db1dc79745e4b23b01af5d34798b7516d9085
Espen Albert <espen.albert@mongodb.com>
2025-08-12 10:48:58 +0100

    feat: Consolidate AWS example configurations into main.tf

--- examples/arch/01_05_full_aws/main.tf
+++ examples/arch/01_05_full_aws/main.tf
@@ -7,7 +7,7 @@ locals {
     "Version"  = "v0.0.0"
     "Email"    = "username@example.com"
   }
-  clusterWithAlertsName = "ClusterPortalProd"
+  cluster_with_alerts_name = "ClusterPortalProd"
   audit_filter_json = jsonencode({
     atype = "authenticate"
     param = {
@@ -30,10 +30,72 @@ module "atlas_project" {
   tags             = local.tags
 }
 
+module "cloud_provider_aws" {
+  source = "../../../modules/04_cloud_provider_aws"
+
+  project_id   = module.atlas_project.id
+  atlas_region = var.atlas_region
+  aws_encryption_at_rest = {
+    enabled                    = true
+    require_private_networking = true
+  }
+  aws_iam_role_name = var.aws_iam_role_name
+}
+
+module "auth_db" {
+  source     = "../../../modules/05_auth_db"
+  project_id = module.atlas_project.id
+  aws_iam_role_database_users = [
+    {
+      role_arn = var.aws_iam_role_arn_ec2
+      roles = [
+        {
+          role_name     = "atlasAdmin"
+          database_name = "admin"
+        }
+      ]
+    }
+  ]
+  custom_db_roles = {
+    my_custom_role = {
+      actions = {
+        UPDATE = [
+          {
+            database_name = "myDb"
+          }
+        ]
+        INSERT = [
+          {
+            database_name = "myDb"
+          }
+        ]
+        REMOVE = [
+          {
+            database_name = "myDb"
+          }
+        ]
+      }
+    }
+  }
+}
+
+module "networking_aws" {
+  source = "../../../modules/06_networking_aws"
+
+  project_id   = module.atlas_project.id
+  atlas_region = var.atlas_region
+  aws_private_endpoint = {
+    vpc_id             = module.vpc.vpc_id
+    subnet_ids         = module.vpc.private_subnets
+    security_group_ids = [aws_security_group.this.id]
+  }
+  tags = local.tags
+}
+
 module "atlas_cluster" {
   source                 = "../../../modules/01_cluster"
   project_id             = module.atlas_project.id
-  name                   = local.clusterWithAlertsName
+  name                   = local.cluster_with_alerts_name
   cluster_type           = "REPLICASET"
   mongo_db_major_version = var.mongodb_version
 
@@ -48,7 +110,40 @@ module "atlas_cluster" {
       region_name   = var.atlas_region
     }]
   }]
-  tags = local.tags
+  encryption_at_rest_provider = "AWS"
+  tags                        = local.tags
+
+  depends_on = [
+    module.cloud_provider_aws, # to create the KMS key
+    module.networking_aws,     # to create the VPC endpoint
+  ]
+}
+
+module "alert_configuration" {
+  source     = "../../../modules/03_alert_configuration"
+  project_id = module.atlas_project.id
+  event_type = "REPLICATION_OPLOG_WINDOW_RUNNING_OUT"
+  enabled    = true
+  matcher = {
+    field_name = "CLUSTER_NAME"
+    operator   = "EQUALS"
+    value      = local.cluster_with_alerts_name
+  }
+  notification = {
+    type_name     = "GROUP"
+    interval_min  = 5
+    delay_min     = 1
+    sms_enabled   = false
+    email_enabled = true
+    roles         = ["GROUP_CLUSTER_MANAGER"]
+  }
+  threshold_config = {
+    operator  = "LESS_THAN"
+    threshold = 1
+    units     = "HOURS"
+  }
+
+  depends_on = [module.atlas_cluster]
 }
 
 module "automated_backup_test_cluster" {
@@ -124,29 +219,4 @@ module "automated_backup_test_cluster" {
 
   backup_enabled = true # enable cloud backup snapshots
   pit_enabled    = true
-}
-
-module "alert_configuration" {
-  source     = "../../../modules/03_alert_configuration"
-  project_id = module.atlas_project.id
-  event_type = "REPLICATION_OPLOG_WINDOW_RUNNING_OUT"
-  enabled    = true
-  matcher = {
-    field_name = "CLUSTER_NAME"
-    operator   = "EQUALS"
-    value      = local.clusterWithAlertsName
-  }
-  notification = {
-    type_name     = "GROUP"
-    interval_min  = 5
-    delay_min     = 1
-    sms_enabled   = false
-    email_enabled = true
-    roles         = ["GROUP_CLUSTER_MANAGER"]
-  }
-  threshold_config = {
-    operator  = "LESS_THAN"
-    threshold = 1
-    units     = "HOURS"
-  }
 }
